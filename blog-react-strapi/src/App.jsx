4d2f25f42164e42251cdc67f947906fc05f91de2
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-11-28 21:25:54 -0300

    Se corrige problema de llamado en config de tailwind. Se agrega archivo para comenzar a unir lo creado en Strapi con lo creado en React.

--- blog-react-strapi/src/App.jsx
+++ blog-react-strapi/src/App.jsx
@@ -1,7 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Homepage, BlogContentpage } from "./pages";
+import useFetch from './hooks/useFetch'
 
 export default function App() {
+  let {loading, data, error} =useFetch('http://localhost:1337/api/blogs?populate=*')
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error!</p>
+  console.log(data);
+
+
   return (
     <div>
       <Routes>
--- blog-react-strapi/tailwind.config.js
+++ blog-react-strapi/tailwind.config.js
@@ -1,7 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  mode: "jit",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "!./src/strapi/**/*" // Excluir la carpeta strapi
+  ],
   theme: {
     extend: {
       colors: {
@@ -24,4 +26,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
